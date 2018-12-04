ea8576b9b11f6fc415ec52bad8ad7303b034da2f
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-04 11:16:53 -0600

    Added a Min function without using for (or math.min)

--- arrloops.js
+++ arrloops.js
@@ -12,3 +12,20 @@ function max(arr){
 
 }
 
+//Min function without for
+
+function min(arr){
+    let currentMin = arr[0];
+    let i = 0;
+    while(i < arr.length){
+        if(arr[i] < currentMin){
+            currentMin = arr[i];  
+        }i++;
+    
+    }
+    return currentMin;
+
+}
+
+const myNum = [10, 3, 6, 2, 3, 100, 20];
+console.log(min(myNum));
